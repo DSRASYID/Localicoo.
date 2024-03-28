32bf89925f580af6a9bf61c2d4aa3818b3399d51
DSRASYID <deasulaeman@gmail.com>
2024-03-28 15:47:11 +0700

    Localicoo.

---
+++ .vscode/js/script.js
@@ -0,0 +1,15 @@
+// tooggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburgermenu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// klik diluar sidebar
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
